9aee7ac4793bdec1cc2c5c7a622534a45317340e
SiniKraft <natclaudix@gmail.com>
2022-03-09 18:19:05 +0100

    Adding update

---
+++ ui_add_app_dialog.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'add_app_dialognjvbtd.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.5
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *  # type: ignore
+from PySide2.QtGui import *  # type: ignore
+from PySide2.QtWidgets import *  # type: ignore
+
+from PySide2.QtWebEngineWidgets import QWebEngineView
+
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(640, 480)
+        self.verticalLayout_2 = QVBoxLayout(Dialog)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        font = QFont()
+        font.setFamily(u"Segoe UI")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setWeight(50)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet(u"QPushButton{ color : rgb(0, 42, 174); text-align: left; }")
+        self.pushButton.setFlat(True)
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.groupBox = QGroupBox(Dialog)
+        self.groupBox.setObjectName(u"groupBox")
+        self.gridLayout = QGridLayout(self.groupBox)
+        self.gridLayout.setSpacing(6)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(1, 1, 1, 1)
+        self.webEngineView = QWebEngineView(self.groupBox)
+        self.webEngineView.setObjectName(u"webEngineView")
+        self.webEngineView.setUrl(QUrl(u"https://duckduckgo.com/"))
+
+        self.gridLayout.addWidget(self.webEngineView, 1, 0, 1, 1)
+
+
+        self.verticalLayout.addWidget(self.groupBox)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.buttonBox = QDialogButtonBox(Dialog)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(False)
+
+        self.verticalLayout_2.addWidget(self.buttonBox)
+
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"Install a new Application !", None))
+        self.groupBox.setTitle("")
+    # retranslateUi
+
